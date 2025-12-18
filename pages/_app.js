4ece8f4d1480a9f0a3fb581d7229955d30b7011c
Batu Eren <batu.eren2004@gmail.com>
2025-12-18 20:28:19 +0100

    custom name

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,41 @@ import Head from 'next/head'
 
 import '../styles/main.css'
 
+const SITE = {
+  name: 'Batu Eren',
+  title: 'Batu Eren | Computer Science @ TU Delft',
+  description:
+    'Computer Science student at TU Delft focused on performance-oriented C++, systems programming, and graphics.',
+  url: 'https://batueren.vercel.app', 
+  ogImage: '/og.png'
+}
+
 export default function Nextra({ Component, pageProps }) {
   return (
     <>
       <Head>
+        {/* Basic SEO */}
+        <title>{SITE.title}</title>
+        <meta name="description" content={SITE.description} />
+        <link rel="canonical" href={SITE.url} />
+
+        {/* Open Graph */}
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content={SITE.name} />
+        <meta property="og:title" content={SITE.title} />
+        <meta property="og:description" content={SITE.description} />
+        <meta property="og:url" content={SITE.url} />
+        <meta property="og:image" content={`${SITE.url}${SITE.ogImage}`} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={SITE.title} />
+        <meta name="twitter:description" content={SITE.description} />
+        <meta name="twitter:image" content={`${SITE.url}${SITE.ogImage}`} />
+
+        {/* RSS + font preload (keep yours) */}
         <link
           rel="alternate"
           type="application/rss+xml"
@@ -21,6 +52,7 @@ export default function Nextra({ Component, pageProps }) {
           crossOrigin="anonymous"
         />
       </Head>
+
       <Component {...pageProps} />
     </>
   )
